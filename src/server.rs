a4448d34e179d9b23b5bb5d55b3ef62ccb470339
loomaclin <loo.maclin@protonmail.com>
2018-03-01 13:08:13 +0300

    check http method, if is not Post, return empty response with status MethodNotAllowed

--- src/server.rs
+++ src/server.rs
@@ -67,7 +67,9 @@ impl<S: Service + JsonService + 'static> Service for JsonServer<S>
 
     fn call(&self, req: Request) -> Self::Future {
         let service = self.inner.clone();
-        Box::new(match service.deserialize(req.path(), req.method()) {
+        Box::new(
+            if *req.method() == Method::Post {
+            match service.deserialize(req.path(), req.method()) {
             Ok(f) => {
                 let req = req.body().concat2()
                             .map_err(move |e| ErrorKind::InternalError(e.to_string()).into())
@@ -93,7 +95,9 @@ impl<S: Service + JsonService + 'static> Service for JsonServer<S>
             Err(e) => {
                 Either::B(future::ok(error_to_response(e)))
             }
-        })
+        } } else {
+                Either::B(future::ok(Response::new().with_status(StatusCode::MethodNotAllowed)))
+            })
     }
 }
 
