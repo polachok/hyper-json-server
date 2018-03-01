56e4c2c7b2be6b9899930e2deab66da00aa1c3fd
Alexander Polyakov <aleksandr.polyakov@kupibilet.ru>
2018-03-01 13:00:54 +0300

    Don't show error chain in response, return error in json

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate futures;
 extern crate hyper;
 extern crate serde;
+#[macro_use]
 extern crate serde_json;
 #[macro_use]
 extern crate error_chain;
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,5 @@
 use hyper;
-use hyper::header::ContentLength;
+use hyper::header::{ContentLength, ContentType};
 use hyper::server::{Request, Response, Service};
 use hyper::{Method, StatusCode};
 use futures::{future, Future, Stream};
@@ -8,7 +8,6 @@ use serde::de::DeserializeOwned;
 use serde::Serialize;
 use serde_json;
 use std::sync::Arc;
-use error_chain::ChainedError;
 
 error_chain! {
     errors {
@@ -37,20 +36,25 @@ fn error_to_response(error: Error) -> Response
 {
     let (status, body) = match error.kind() {
         &ErrorKind::NotFound(_) => {
-            (StatusCode::NotFound, format!("{}", error.display_chain()))
+            (StatusCode::NotFound, format!("{}", error))
         },
         &ErrorKind::BadRequest(_) => {
-            (StatusCode::BadRequest, format!("{}", error.display_chain()))
+            (StatusCode::BadRequest, format!("{}", error))
         },
         &ErrorKind::InternalError(_) => {
-            (StatusCode::InternalServerError, format!("{}", error.display_chain()))
+            (StatusCode::InternalServerError, format!("{}", error))
         },
-        _ => (StatusCode::InternalServerError, format!("{}", error.display_chain()))
+        _ => (StatusCode::InternalServerError, format!("{}", error))
     };
+    let resp = json!({
+        "error": body,
+    });
+    let body = resp.to_string();
     let body_len = body.len() as u64;
     Response::new()
         .with_body(body)
         .with_header(ContentLength(body_len))
+        .with_header(ContentType::json())
         .with_status(status)
 }
 
@@ -76,6 +80,7 @@ impl<S: Service + JsonService + 'static> Service for JsonServer<S>
                                 let resp = Response::new()
                                     .with_body(body)
                                     .with_header(ContentLength(len))
+                                    .with_header(ContentType::json())
                                     .with_status(StatusCode::Ok);
                                 future::ok(resp)
                             },
